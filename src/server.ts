1f11374f8ae184c0ade6f10a9272f7cd4c9c47ae
unknown <aline.as385@gmail.com>
2020-11-03 14:32:42 -0300

    Tratativa de erros global

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,36 @@
 import 'reflect-metadata';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.directory)); // Visualizar arquivos em localhost:3333/nomeDoArquivo
 app.use(routes);
+
+// Tratativa de erros global
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server stared on port 3333!');
