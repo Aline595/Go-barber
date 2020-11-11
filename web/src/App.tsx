33b7ad62a855efe9864716f64210f9e0e8c970d4
unknown <aline.as385@gmail.com>
2020-11-11 14:55:14 -0300

    Página de login

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
+import Signin from './pages/Signin/index';
 
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <Signin />
     <GlobalStyle />
   </>
 );
